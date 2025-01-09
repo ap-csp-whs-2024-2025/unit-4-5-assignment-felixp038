9f0f64f75d0c08a29de7c03282235caf489749c1
felixp038 <phanfelix1@gmail.com>
2025-01-09 22:36:03 +0000

    update2

--- homework.cpp
+++ homework.cpp
@@ -8,8 +8,16 @@ void displayList(std::vector<T> const &v);
 int main()
 {
     // You can follow along with any code in class here
+    std::vector<int> numbers = {1, 2, 3, 4, 5};
+    std::vector<double> decimals = {3.14, 1.59, 2.65, 1.23, 2.08};
+    std::vector<std::string> words = {"hello", "darkness", "friend"};
+    std::cout << numbers[0] << ", " <<  numbers[2] << ", " << numbers[4] << std::endl;
+    std::cout << words[0] << std::endl;
+    words.push_back("bada bing");
+    displayList(words);
+    decimals.pop_back();
+    displayList(decimals);
 
-    
     return 0;
 }
 
